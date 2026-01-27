f9150296ee4d8ab5a1dc01193b7aa060dad1053f
damnnou <skdamn@outlook.com>
2026-01-27 18:31:50 +0300

    v1.0.16 (change integral graphql)

--- src/config/graphql-urls.ts
+++ src/config/graphql-urls.ts
@@ -4,5 +4,6 @@ import { SupportedChainId } from '../types';
 export const ALM_GRAPH_URL: Record<SupportedChainId, string | 'none'> = {
   [SupportedChainId.Base]:
     'https://gateway.thegraph.com/api/4d7b59e4fd14365ae609945af85f3938/subgraphs/id/JBPEDHbBjmDWNM1atSS6UkWPQi5HonjDU3c923vRTLqC', // clamm 1.2.1
-  [SupportedChainId.BaseSepolia]: 'https://api.studio.thegraph.com/query/82608/ve-alm/version/latest', // integral 1.2.2 farming + alm + ve33
+  [SupportedChainId.BaseSepolia]:
+    'https://api.goldsky.com/api/public/project_cm2cd1yfmmrav01u9b02f69vj/subgraphs/integral-ve-alm/v1.0.0/gn', // integral 1.2.2 farming + alm + ve33
 };
